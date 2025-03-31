b7bd06637a2c6d9dc2d567502daf2521d34463af
unknown <sukanya.madupu@gmail.com>
2025-03-30 22:16:36 -0400

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { WordcountComponent } from './wordcount/wordcount.component';
-import { CorrectionsComponent } from './corrections/corrections.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent, pathMatch: 'full' },
-    // { path: 'wordCount', component: WordcountComponent },
-    // { path: 'corrections', component: CorrectionsComponent },
 ];
